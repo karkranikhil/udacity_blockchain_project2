2d5d27272e36c14e668f826e6c9a9200f530f4e7
Luke Buthman <lbuthman@gmail.com>
2018-06-24 07:06:29 -0500

    [chore] add leveldb and test statements for easier testing

--- simpleChain.js
+++ simpleChain.js
@@ -60,7 +60,7 @@ class Blockchain{
     // validate block
     validateBlock(blockHeight){
       // get block object
-      let block = this.getBlock(blockHeight);
+      let block = getLevelDBData(blockHeight);
       // get block hash
       let blockHash = block.hash;
       // remove block hash to test block integrity
@@ -97,3 +97,42 @@ class Blockchain{
       }
     }
 }
+
+/* ===== Persist data with LevelDB ===================================
+|  Learn more: level: https://github.com/Level/level     |
+|  =============================================================*/
+
+const level = require('level');
+const chainDB = './chaindata';
+const db = level(chainDB);
+
+// Add data to levelDB with key/value pair
+function addLevelDBData(key,value){
+  db.put(key, value, function(err) {
+    if (err) return console.log('Block ' + key + ' submission failed', err);
+  })
+}
+
+// Get data from levelDB with key
+function getLevelDBData(key){
+  db.get(key, function(err, value) {
+    if (err) return console.log('Not found!', err);
+    console.log('Value = ' + value);
+  })
+}
+
+// Add data to levelDB with value
+function addDataToLevelDB(value) {
+    let i = 0;
+    db.createReadStream().on('data', function(data) {
+          i++;
+        }).on('error', function(err) {
+            return console.log('Unable to read data stream!', err)
+        }).on('close', function() {
+          console.log('Block #' + i);
+          addLevelDBData(i, value);
+        });
+}
+
+const blockchain = new Blockchain();
+blockchain.addBlock(new Block('Test 2'));
